bc347c718a5cc056f7869ba6270e0e2c76501a68
Kate Trahan and Paul Kotok <kate@>
2017-11-27 11:04:12 -0800

    add baisick html

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var add = function (number1, number2, number3) {
+return (number1 + number2) *number3;
+};
+
+var number1 = parseInt(prompt("Enter a number here."));
+var number2 = parseInt(prompt("Enter another number here"));
+var number3 = parseInt(prompt("Enter your favorite number here"));
+
+alert(add(number1,number2, number3));
